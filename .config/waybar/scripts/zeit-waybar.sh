87f38431cf23b0713e63dc5c421cbb85e2c2e7c8
fabirez <skyc4ff3ine@virgilio.it>
2026-07-20 23:29:28 +0200

    zeit status in waybar

--- .config/waybar/scripts/zeit-waybar.sh
+++ .config/waybar/scripts/zeit-waybar.sh
@@ -4,6 +4,14 @@ ZEIT_BIN=zeit
 statusOut=$($ZEIT_BIN --format json)
 
 
+# {
+#   "status": "tracking",
+#   "is_running": true,
+#   "project_sid": "test",
+#   "task_sid": "dev",
+#   "timer": 302
+# }
+
 # Getting all the keys 
 #		if is running     (is_running, status, project_sid, task_sid, timer)
 #		Otherwise         (is_running, status)
@@ -21,8 +29,21 @@ if [[ "$is_running" == true ]]; then
 
 	# --[c]ompact-output  format the output on one line.
 	# --[n]ull-input since we have no input, but we are creating the json object from scratch
-	jq -nc \
-		'{class:"active"}'
+	# jq -nc \
+	# 	'{class:"active"}'
+  #
+
+  hours=$((timer / 3600))
+  minutes=$(((timer % 3600) / 60))
+  seconds=$((timer % 60))
+  timer_fmt=$(printf "%02d:%02d:%02d" "$hours" "$minutes" "$seconds")
+# complete e.g with all the args
+  jq -nc \
+      --arg project_sid "$project_sid" \
+      --arg task_sid "$task_sid" \
+      --arg timer_fmt "$timer_fmt" \
+  '{ text:"Working on: " + $project_sid + "/" + $task_sid + " " + $timer_fmt, class:"active" }'
+
 fi
 
 # complete e.g with all the args
